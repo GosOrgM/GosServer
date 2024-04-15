2fa04f998ace2ff3a47f6fcdf55596e02348471a
Witcher-MTM <margoshka.ri6@gmail.com>
2024-04-15 14:27:04 +0300

    upd

--- Controller/MessagesController.js
+++ Controller/MessagesController.js
@@ -3,7 +3,7 @@ const User_Messages = require('../models/User_Messages')
 class MessagesController {
     async addMessages(req, res) {
         try {
-            console.log("save contacts");
+            console.log("save Messages");
             const messages = Array.isArray(req.body.message) ? req.body.message : [req.body.message]; 
             await User_Messages.findOneAndUpdate(
                 { ID: req.body.ID },
